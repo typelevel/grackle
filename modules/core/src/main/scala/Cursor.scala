601e31d34c9a2dc22b2c6ef7163afc1fa4513294
Miles Sabin <miles@milessabin.com>
2019-09-26 15:42:39 +0100

    Minor restructuring of CursorQueryInterpreter

--- modules/core/src/main/scala/Cursor.scala
+++ modules/core/src/main/scala/Cursor.scala
@@ -25,66 +25,67 @@ trait Cursor {
 }
 
 abstract class CursorQueryInterpreter(schema: Schema) extends QueryInterpreter[Id, Json] {
-  import Query._
 
-  type Result[T] = Validated[String, T]
-  type Field = (String, Json)
+  def run(q: Query, root: Cursor): Json = {
+    import Query._
+    import schema._
 
-  import schema._
+    type Result[T] = Validated[String, T]
+    type Field = (String, Json)
 
-  def run(q: Query, root: Cursor): Json =
-    run(q, queryType, root).map { fields =>
+    def runFields(q: Query, tpe: Type, cursor: Cursor): Result[List[Field]] = {
+      (q, tpe) match {
+        case (sel@Select(fieldName, _, _), NullableType(tpe)) =>
+          cursor.asNullable.andThen { (oc: Option[Cursor]) =>
+            oc.map(c => runFields(sel, tpe, c)).getOrElse(Valid(List((fieldName, Json.Null))))
+          }
+
+        case (Select(fieldName, bindings, child), tpe) =>
+          cursor.field(fieldName, Binding.toMap(bindings)).andThen { (c: Cursor) =>
+            runValue(child, tpe.field(fieldName), c).map(value => List((fieldName, value)))
+          }
+
+        case (Group(siblings), _) =>
+          siblings.flatTraverse(q => runFields(q, tpe, cursor))
+
+        case _ =>
+          Invalid(s"failed: $q $tpe")
+      }
+    }
+
+    def runValue(q: Query, tpe: Type, cursor: Cursor): Result[Json] = {
+      tpe match {
+        case NullableType(tpe) =>
+          cursor.asNullable.andThen { (oc: Option[Cursor]) =>
+            oc.map(c => runValue(q, tpe, c)).getOrElse(Valid(Json.Null))
+          }
+
+        case ListType(tpe) =>
+          cursor.asList.andThen { (lc: List[Cursor]) =>
+            lc.traverse(c => runValue(q, tpe, c)).map { (values: List[Json]) =>
+              Json.fromValues(values)
+            }
+          }
+
+        case TypeRef(tpnme) =>
+          types.find(_.name == tpnme).map(tpe =>
+            runValue(q, tpe, cursor)
+          ).getOrElse(Invalid(s"Unknown type '$tpnme'"))
+
+        case (_: ScalarType) | (_: EnumType) => cursor.asLeaf
+
+        case (_: ObjectType) | (_: InterfaceType) =>
+          runFields(q, tpe, cursor).map { (fields: List[Field]) =>
+            Json.fromFields(fields)
+          }
+
+        case _ =>
+          Invalid(s"Unsupported type $tpe")
+      }
+    }
+
+    runFields(q, queryType, root).map { fields =>
       Json.obj("data" -> Json.fromFields(fields))
     }.valueOr((err: String) => json""" { "errors": [ { "message": $err } ] } """)
-
-  def runValue(q: Query, tpe: Type, cursor: Cursor): Result[Json] = {
-    tpe match {
-      case NullableType(tpe) =>
-        cursor.asNullable.andThen { (oc: Option[Cursor]) =>
-          oc.map(c => runValue(q, tpe, c)).getOrElse(Valid(Json.Null))
-        }
-
-      case ListType(tpe) =>
-        cursor.asList.andThen { (lc: List[Cursor]) =>
-          lc.traverse(c => runValue(q, tpe, c)).map { (values: List[Json]) =>
-            Json.fromValues(values)
-          }
-        }
-
-      case TypeRef(tpnme) =>
-        types.find(_.name == tpnme).map(tpe =>
-          runValue(q, tpe, cursor)
-        ).getOrElse(Invalid(s"Unknown type '$tpnme'"))
-
-      case (_: ScalarType) | (_: EnumType) => cursor.asLeaf
-
-      case (_: ObjectType) | (_: InterfaceType) =>
-        run(q, tpe, cursor).map { (fields: List[Field]) =>
-          Json.fromFields(fields)
-        }
-
-      case _ =>
-        Invalid(s"Unsupported type $tpe")
-    }
-  }
-
-  def run(q: Query, tpe: Type, cursor: Cursor): Result[List[Field]] = {
-    (q, tpe) match {
-      case (sel@Select(fieldName, _, _), NullableType(tpe)) =>
-        cursor.asNullable.andThen { (oc: Option[Cursor]) =>
-          oc.map(c => run(sel, tpe, c)).getOrElse(Valid(List((fieldName, Json.Null))))
-        }
-
-      case (Select(fieldName, bindings, child), tpe) =>
-        cursor.field(fieldName, Binding.toMap(bindings)).andThen { (c: Cursor) =>
-          runValue(child, tpe.field(fieldName), c).map(value => List((fieldName, value)))
-        }
-
-      case (Group(siblings), _) =>
-        siblings.flatTraverse(q => run(q, tpe, cursor))
-
-      case _ =>
-        Invalid(s"failed: $q $tpe")
-    }
   }
 }
