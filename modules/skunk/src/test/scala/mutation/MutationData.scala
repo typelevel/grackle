62d3c746fe21b9842757511b5fb7776f8607a48d
Rob Norris <rob_norris@mac.com>
2021-03-29 10:01:26 -0500

    tests

---
+++ modules/skunk/src/test/scala/mutation/MutationData.scala
@@ -0,0 +1,141 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package mutation
+
+import cats.effect.{ Bracket, Resource, Sync }
+import cats.syntax.all._
+import edu.gemini.grackle._
+import edu.gemini.grackle.QueryCompiler._
+import edu.gemini.grackle.Query._
+import edu.gemini.grackle.Value._
+import edu.gemini.grackle.Predicate._
+import edu.gemini.grackle.skunk._
+import _root_.skunk.Session
+import _root_.skunk.implicits._
+import _root_.skunk.codec.all._
+import cats.data.IorT
+
+trait MutationSchema[F[_]] extends SkunkMapping[F] {
+
+  class TableDef(name: String) {
+    def col(colName: String, codec: Codec[_]): ColumnRef =
+      ColumnRef(name, colName, codec)
+  }
+
+  object country extends TableDef("country") {
+    val code = col("code", bpchar(3))
+    val name = col("name", text)
+  }
+
+  object city extends TableDef("city") {
+    val id          = col("id", int4)
+    val countrycode = col("countrycode", bpchar(3))
+    val name        = col("name", text)
+    val population  = col("population", int4)
+  }
+
+}
+
+trait MutationMapping[F[_]] extends MutationSchema[F] {
+
+  implicit def ev: Bracket[F, Throwable]
+
+  val schema =
+    Schema(
+      """
+        type Query {
+          city(id: Int!): City
+        }
+        type Mutation {
+          updatePopulation(id: Int!, population: Int!): City
+        }
+        type City {
+          name: String!
+          country: Country!
+          population: Int!
+        }
+        type Country {
+          name: String!
+          cities: [City!]!
+        }
+      """
+    ).right.get
+
+  val QueryType    = schema.ref("Query")
+  val MutationType = schema.ref("Mutation")
+  val CountryType  = schema.ref("Country")
+  val CityType     = schema.ref("City")
+
+  val typeMappings =
+    List(
+      ObjectMapping(
+        tpe = QueryType,
+        fieldMappings = List(
+          SqlRoot("city"),
+        ),
+      ),
+      ObjectMapping(
+        tpe = MutationType,
+        fieldMappings = List(
+          SqlRoot("updatePopulation", mutation = Mutation.unit { (_, e) =>
+            (e.get[Int]("id"), e.get[Int]("population")).tupled match {
+              case None =>
+                QueryInterpreter.mkErrorResult(s"Implementation error, expected id and population in $e.").pure[F]
+              case Some((id, pop)) =>
+                pool.use { s =>
+                  s.prepare(sql"update city set population=$int4 where id=$int4".command).use { ps =>
+                    IorT.right(ps.execute(pop ~ id).void).value // awkward
+                  }
+                }
+              }
+          }),
+        ),
+      ),
+       ObjectMapping(
+        tpe = CountryType,
+        fieldMappings = List(
+          SqlAttribute("code", country.code, key = true),
+          SqlField("name",     country.name),
+          SqlObject("cities",  Join(country.code, city.countrycode)),
+        ),
+      ),
+      ObjectMapping(
+        tpe = CityType,
+        fieldMappings = List(
+          SqlAttribute("id", city.id, key = true),
+          SqlAttribute("countrycode", city.countrycode),
+          SqlField("name", city.name),
+          SqlField("population", city.population),
+          SqlObject("country", Join(city.countrycode, country.code)),
+        )
+      ),
+    )
+
+  override val selectElaborator = new SelectElaborator(Map(
+    QueryType -> {
+      case Select("city", List(Binding("id", IntValue(id))), child) =>
+        Select("city", Nil, Unique(Eql(AttrPath(List("id")), Const(id)), child)).rightIor
+    },
+    MutationType -> {
+      case Select("updatePopulation", List(Binding("id", IntValue(id)), Binding("population", IntValue(pop))), child) =>
+        Environment(
+          Cursor.Env("id" -> id, "population" -> pop),
+          Select("updatePopulation", Nil,
+            // We could also do this in the SqlRoot's mutation, and in fact would need to do so if
+            // the mutation generated a new id. But for now it seems easiest to do it here.
+            Unique(Eql(AttrPath(List("id")), Const(id)), child)
+          )
+        ).rightIor
+    }
+  ))
+}
+
+object MutationMapping extends SkunkMappingCompanion {
+
+  def mkMapping[F[_]: Sync](pool: Resource[F,Session[F]], monitor: SkunkMonitor[F]): Mapping[F] =
+    new SkunkMapping[F](pool, monitor) with MutationMapping[F] {
+      val ev = Sync[F]
+    }
+
+}
---
+++ modules/skunk/src/test/scala/mutation/SqlMutationSpec.scala
@@ -0,0 +1,71 @@
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package mutation
+
+import org.scalatest.funsuite.AnyFunSuite
+import edu.gemini.grackle.QueryExecutor
+import cats.effect.IO
+import io.circe.Json
+import io.circe.literal._
+
+trait SqlMutationSpec extends AnyFunSuite {
+
+  def mapping: QueryExecutor[IO, Json]
+
+  def check(query: String, expected: Json) =
+    assert(mapping.compileAndRun(query).unsafeRunSync() == expected)
+
+  test("sanity check") {
+    check("""
+        query {
+          city(id: 1) {
+            name
+            population
+          }
+        }
+      """,
+      json"""
+        {
+          "data": {
+            "city": {
+              "name": "Kabul",
+              "population" : 1780000
+            }
+          }
+        }
+      """
+    )
+  }
+
+
+  test("simple update") {
+    check("""
+        mutation {
+          updatePopulation(id: 2, population: 12345) {
+            name
+            population
+            country {
+              name
+            }
+          }
+        }
+      """,
+      json"""
+        {
+          "data" : {
+            "updatePopulation" : {
+              "name" : "Qandahar",
+              "population" : 12345,
+              "country" : {
+                "name" : "Afghanistan"
+              }
+            }
+          }
+        }
+      """
+    )
+  }
+
+
+}
