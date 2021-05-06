08592d44deef94adf9f92f2661ce9f8aac036266
Rob Norris <rob_norris@mac.com>
2021-05-06 09:22:13 -0500

    undo header update

--- modules/doobie/src/test/scala/mutation/MutationSpec.scala
+++ modules/doobie/src/test/scala/mutation/MutationSpec.scala
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
 package mutation
